7961b96dad1c84736d8652a245314f969e4eb28d
nikoloz2406 <nikoloz2406@gmail.com>
2025-11-02 19:42:21 +0400

    first commit

---
+++ class.py
@@ -0,0 +1,42 @@
+item = "car"
+name = "nikoloz"
+city = "tbilisi"
+number = "24"
+app = "dicord"
+
+print(item)
+print(name)
+print(city)
+print(number)
+print(app)
+
+age = 7
+day = 12
+year = 2025
+month = 11
+cnture = 21
+
+
+print(day)
+print(age)
+print(year)
+print(month)
+print(centure)
+
+caliber = "8.2"
+kg = "40.5"
+money = "74.2"
+km = "50.1"
+cm = "4.7"
+
+print(caliber)
+print(kg)
+print(money)
+print(km)
+print(cm)
+
+points = 35
+points = 45
+print(points)
+
+
---
+++ day04.py
@@ -0,0 +1,37 @@
+Name = "dashi"
+print(Name)
+
+Color = "blue"
+print(Color)
+
+Age = "10" 
+print(Age)
+
+Animal = "lion"
+print(Animal) 
+
+Toy = "lego" 
+print(Toy)
+
+movie = "titanic"
+year = 1997
+print(movie)
+
+band = "beatles"
+song = "yesterday"
+song = "let it be"
+print(song)
+
+items = 18
+boxes = 6
+print(items / boxes)
+
+#print("game over"
+
+# client = "6"
+print(client)
+
+credit = 300
+Credit = 280
+CREDIT = 320
+print(CREDIT)
