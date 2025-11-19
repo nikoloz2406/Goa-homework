1147005793d021025098f7ef80f22ff93ebeb82f
nikoloz2406 <nikoloz2406@gmail.com>
2025-11-19 22:21:30 +0400

    second commit

---
+++ day 06/cw 006/cw006.py
@@ -0,0 +1,15 @@
+year  02 = 2
+print(2)
+
+day 03 = 3
+print(3)
+
+week 021 =21
+print(21)
+
+month 091 =91
+print(91)
+
+dog 0mops =0
+print(0)
+
---
+++ hw006.py
@@ -0,0 +1,9 @@
+0day = saturday
+print(0day)
+
+my name = nikoloz
+print(my name)
+
+favorite*sport = basketball
+print(favorite*sport)
+
