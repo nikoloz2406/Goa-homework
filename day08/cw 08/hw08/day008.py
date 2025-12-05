2bcd3e2dcd6030b84f2095179b9687f40d887630
nikoloz2406 <nikoloz2406@gmail.com>
2025-12-05 16:34:47 +0400

    second commit

---
+++ day08/cw 08/hw08/day008.py
@@ -0,0 +1,14 @@
+name = input("enter your name")#input
+surname = input("enter your surname")#input
+age = input("enter your age")#input
+
+print(name)
+print(surname)
+print(age)
+
+
+
+name = input("enter your name : ")
+print(name)
+
+print(input("enter your name"))
