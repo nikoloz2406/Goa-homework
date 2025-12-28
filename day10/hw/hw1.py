96316ca24040617b3de9437e4d570328fba7ebcc
nikoloz2406 <nikoloz2406@gmail.com>
2025-12-28 10:54:41 +0400

    second commit

--- day10/hw/hw1.py
+++
@@ -1,4 +0,0 @@
-salary = 1000
-pay_raise = 100
-new_salary = salary + pay_raise
-print(salary)
---
+++ day10/hw/hw10.py
@@ -0,0 +1,13 @@
+name = input("enter your name :")
+print(name * 2)
+
+
+surname = input("hello enter your surname :")
+print(surname)
+
+
+name = input("enter your name :")
+surname = input("enter your surname :")
+age = input("enter your age :")
+
+print(name + surname + age)
