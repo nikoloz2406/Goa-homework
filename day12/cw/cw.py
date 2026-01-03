bdb99b1b725aa7adc0b83956647e25e103c21841
nikoloz2406 <nikoloz2406@gmail.com>
2026-01-03 13:04:11 +0400

    second commit

---
+++ day12/cw/cw.py
@@ -0,0 +1,13 @@
+num1 = input("enter any number :")
+num2 = input("enter any numer :")
+
+print(num1)
+print(num2)
+
+print(num1 + num2)
+
+
+
+
+
+
---
+++ day12/hw/hw.py
@@ -0,0 +1,44 @@
+num1 = int(input("enter any number :"))
+num2 = int(input("enter any number :"))
+num3 = int(input("enter any number :"))
+
+print(num1)
+print(num2)
+print(num3)
+print(num3 * num2 - num1)
+
+
+
+num1 = int(input("enter any number :"))
+num2 = int(input("enter any number :"))
+
+print(num1)
+print(num2)
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+
+
+
+
+name = input("hello  enter your name :")
+age = input("hello  enter your age :")
+year = input("hello enter your birthday year :")
+
+print(name)
+print(age)
+print(year)
+
+age = input(" enter your age :")
+year = input(" enter your birthday year :")
+
+print(age)
+print(year)
+print #(I am 10 year . born in 2015)
+
+
+age = input("enter your age ")
+print(age)
+print #(after 10 years shy will by 30 years)
