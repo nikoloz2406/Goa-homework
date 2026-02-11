ea527af033f64ae3d10689a14f1b156ada0fb272
nikoloz2406 <nikoloz2406@gmail.com>
2026-02-11 21:00:33 +0400

    second commit

---
+++ day18/hw/hw.py
@@ -0,0 +1,15 @@
+num = 0
+while   num <= 10 :
+    print(num)
+    num +=1
+
+ 
+num = 2
+while   num <= 20 :
+    print(num)
+    num +=2
+
+
+
+pasword = input("enter your pasword:")
+print(pasword)
