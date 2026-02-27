8087ddb6e6372434b36c02ee1054199c81302496
nikoloz2406 <nikoloz2406@gmail.com>
2026-02-27 18:07:44 +0400

    second commit

---
+++ day 20/cw/cw.py
@@ -0,0 +1,9 @@
+age = 23
+
+if age >= 18:
+ print("regulare prise")
+else :
+   print("age < 18")
+  
+    
+    
---
+++ day 20/hw/hw.py
@@ -0,0 +1,21 @@
+num = input("enter any number")
+print(num)
+
+if  num > 0:
+ print("num > 0")
+else :
+   print("num < 0")
+
+
+for i in range(11):
+    print(i)
+
+
+
+password = input("enter password ")
+print(password) 
+
+if password == "goa123":
+ print("password is correct")
+else :
+   print("password is not correct")
