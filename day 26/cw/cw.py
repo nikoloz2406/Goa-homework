874084ac44166b95db05b90e2e8f02093b52f210
nikoloz2406 <nikoloz2406@gmail.com>
2026-04-11 09:04:43 +0400

    second commit

---
+++ day 26/cw/cw.py
@@ -0,0 +1,13 @@
+     
+item = ["pen","bug","pensil","ruller","book"]
+print(item[0])
+print(item[-4])
+
+
+
+name = input("enter your name:")
+print(name)
+
+for i in range(10):
+    print(name+str(i))
+
