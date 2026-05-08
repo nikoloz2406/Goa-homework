f34dd5e48e00b750d59a3d5315e7cdbb53667958
nikoloz2406 <nikoloz2406@gmail.com>
2026-05-08 08:40:05 +0400

    second commit

---
+++ cs.py
@@ -0,0 +1,28 @@
+mag= input("enter onli 5 num:")
+print(mag)
+
+mag = ["1","6","11","74","253"]
+print(mag[-2])
+print(mag[-5])
+
+
+num = input("enter any number")
+print(num)
+
+if  num > 0:
+ print("num არის მეტი 0 ზე")
+else :
+   print("num ᲐᲠᲘᲡ ᲜᲐᲙᲚᲔᲑᲘ 0 ᲖᲔ")
+
+
+
+password = input("enter password")
+print(password)
+
+if password =="goa is best":
+ print("Password is correct!")
+else :
+   print("Incorrect password!")
+
+for i in range(11):
+ print(i) 
---
+++ hk1.py
@@ -0,0 +1,63 @@
+a = int(input("enter any num:"))
+b = int(input("enter any num:"))
+result = (a+b)
+print(result)
+
+
+
+a = int(input("enter any num:"))
+b = int(input("enter any num:"))
+result = (a+b)
+print(result)
+
+
+a = int(input("enter any num:"))
+b = int(input("enter any num:")) 
+c = int(input("enter any num:")) 
+result = (a+b+c)
+print(result)
+
+ 
+a = int(input("True when a = False, b = True "))
+b = int(input(" False when a = False, b = False:")) 
+result = (a or b)
+          
+true= int(input( "is_even"(4) == True))
+false= int(input("is_even"(7) == False))
+true= int(input("is_even"(4) == True ))
+print(true)
+print(false)
+print(true)
+
+
+a = int(input("True when a = False, b = True "))
+b = int(input(" False when a = False, b = False:"))
+result = ("a is greater than b")
+print(result)
+
+for i in range(7):
+ print(i)
+
+num = 0
+while   num <= 5 :
+    print(num)
+    num +=1
+
+a = (input(" enter any num:"))
+if a > 0 :
+ print("a is greater than 0")
+else:
+ print( "a is not greater than 0")
+
+for i in range(10):
+ print(i)
+ result = ("for i in range")
+
+
+item =["pen","pencil","ruler"]
+print(-1)
+
+
+
+item =["pen","pencil","ruler","ireiser","book"]
+print(-3)
