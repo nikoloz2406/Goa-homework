dcc43bcb541a1d745eff501ada84e2d43c589239
nikoloz2406 <nikoloz2406@gmail.com>
2026-05-13 17:43:09 +0400

    second commit

---
+++ day28/cw/cw.py
@@ -0,0 +1,31 @@
+num = ["1","2","3","4","5"]
+print(num[-1])
+print(num[2])
+
+
+
+nums = ["10","20", "30", "40", "50", "60", "70", "80", "90", "100"]
+print(nums[4])
+print(nums[-1])
+print(nums[3:7])
+print(nums[0:9:3])
+
+
+students = ["Alex", "Ben", "Chris", "David", "Elena", "Frank", "George","hannah"]
+print(students[0:1:2])
+print(students[1:2])
+print(students[2:3])
+print(students[0:1:2])
+print(students[1:2])
+print(students[4:5])
+print(students[6:7])
+
+print(students[0:1:2])
+print(students[1:2])
+print(students[4:5])
+print(students[6:7])
+print(students[7:8])
+
+
+print(students[4:5])
+print(students[6:7])
