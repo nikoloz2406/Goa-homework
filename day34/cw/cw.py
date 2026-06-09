a65beb25964f749c1f05792bb45046ce5d6de9f7
nikoloz2406 <nikoloz2406@gmail.com>
2026-06-09 21:58:04 +0400

    second commit

---
+++ day34/cw/cw.py
@@ -0,0 +1,13 @@
+names = ["nikoloz", "dachi", "giorgi"]
+names[2] = "group78"
+print(names)
+
+
+animals = ["carpet", "dog", "lion"]
+animals[0] = "cat"
+print(animals)
+
+
+word= ["c", "o", "t"]
+word[1] = "a"
+print(word)
---
+++ day34/hw/hw.py
@@ -0,0 +1,14 @@
+groups = ['group75', 'group76', 'group77', 'group79']
+groups[3] = "group78"
+print(groups)
+
+
+fruits = ["apple", "banana", "orange", "strawberry", "grape", "mango", "pineapple"]
+fruits[0] = "carrot"
+fruits[1] = "broccoli"
+fruits[2] = "spinach"
+print(fruits)
+print(fruits)
+print(fruits)
+
+
