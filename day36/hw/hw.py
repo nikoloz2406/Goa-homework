3174a000606a7a1c7142962a148094e1502b287f
nikoloz2406 <nikoloz2406@gmail.com>
2026-06-30 13:31:00 +0400

    second commit

---
+++ day36/hw/hw.py
@@ -0,0 +1,46 @@
+
+colors = ["წითელი", "მწვანე", "ლურჯი", "ყვითელი", "იასამნისფერი"]
+print(colors[0])
+print(colors[-1])
+print(colors[2])
+
+
+numbers = ["10", "20", "30", "40", "50", "60","70","80","90","100"]
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+print(numbers[6])
+
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[0])
+print(letters[2])
+print(letters[4])
+print(letters[6])
+print(letters[8])
+
+print(letters[0])
+print(letters[1])
+print(letters[3])
+print(letters[5])
+print(letters[7])
+print(letters[8])
+print(letters[9])
+
+word = ["პ", "ი", "თ", "ო", "ნ", "ი"]
+print(word[5])
+print(word[4])
+print(word[3])
+print(word[2])
+print(word[1])
+print(word[0])
+
+
+animals = ["ძაღლი", "კატა", "თაგვი", "დათვი", "მგელი"]
+print(animals[0])
+animals[1] = "ლომი"
+print(animals)
+animals[2] = "ვეფხვი"
+print(animals[4])
+
