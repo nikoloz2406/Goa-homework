b729b43bc17f2e29c6a47201724887e77e1aa5a0
nikoloz2406 <nikoloz2406@gmail.com>
2026-07-13 11:07:15 +0400

    second commit

---
+++ day38/cw/cw.py
@@ -0,0 +1,18 @@
+secret = ["პ", "ი", "გ", "თ", "ე", "ო", "ნ", "ი", "ც", "ი"]
+
+print(secret[2])
+print(secret[3])
+print(secret[4])
+print(secret[5])
+print(secret[6])
+print(secret[7])
+print(secret[8])
+print(secret[9])
+
+print(secret[3])
+print(secret[4])
+print(secret[5])
+print(secret[6])
+
+print(secret[-2])
+print(secret[-1])
